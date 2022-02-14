2ce114009650b4aeb054c7be686742dd1d3af41f
Aaron <adebejer@mtu.edu>
2022-02-14 09:42:12 -0500

    Annotated fields on Player.

--- src/main/java/com/perscholas/scorekeeper/entity/Player.java
+++ src/main/java/com/perscholas/scorekeeper/entity/Player.java
@@ -1,5 +1,6 @@
 package com.perscholas.scorekeeper.entity;
 
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
@@ -12,16 +13,28 @@ import java.util.List;
 @Entity
 @Table(name = "player")
 public class Player {
+	public final static int TEXT_FIELD_LENGTH = 20;
+	public final static int PASSWORD_LENGTH = 71;
+	public final static int EMAIL_LENGTH = 254;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	@Column(name = "username", unique = true, nullable = false, length = TEXT_FIELD_LENGTH)
+	@NotNull
 	private String username;
+	@Column(name = "password", nullable = false, length = PASSWORD_LENGTH)
+	@NotNull
 	private String password;
+	@Column(name = "email", unique = true, length = EMAIL_LENGTH)
 	private String email;
+	@Column(name = "first_name", length = TEXT_FIELD_LENGTH)
 	private String firstName;
+	@Column(name = "last_name", length = TEXT_FIELD_LENGTH)
 	private String lastName;
-	private List<Game> gamesList;
+	// Linking Players to Games is already done in Game.java.
+	//private List<Game> gamesList;
 
 	public Player() {}
 
